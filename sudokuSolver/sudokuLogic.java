4b1b6961adb20cbd17a859194a49746f2afeeb9c
Ryan Tran <tranryan005@gmail.com>
2025-10-19 22:47:41 -0400

    Creating logic framework

---
+++ sudokuSolver/sudokuLogic.java
@@ -0,0 +1,19 @@
+public class sudokuLogic {
+    public int[] columnFinder() {
+
+    }
+
+    public int[] rowFinder() {
+
+    }
+
+    public int[] blockFinder() {
+
+    }
+
+    public int[] cellAndEr() {
+
+    }
+
+    
+}
