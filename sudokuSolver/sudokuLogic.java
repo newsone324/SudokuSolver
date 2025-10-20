144fc69d8e0728387b1306add6aa20405b8effa7
Ryan Tran <tranryan005@gmail.com>
2025-10-20 01:00:32 -0400

    Added a constructor

--- sudokuSolver/sudokuLogic.java
+++ sudokuSolver/sudokuLogic.java
@@ -1,18 +1,28 @@
 public class sudokuLogic {
-    public int[] columnFinder() {
+    int[][] puzzle;
 
+    /** 
+     * logic constructor
+     * 
+     */
+    sudokuLogic(int[][] inPuzzle) {
+        puzzle = inPuzzle;
+    }
+
+    public int[] columnFinder() {
+        return null;
     }
 
     public int[] rowFinder() {
-
+        return null;
     }
 
     public int[] blockFinder() {
-
+        return null;
     }
 
     public int[] cellAndEr() {
-
+        return null;
     }
 
     
